877a00f268071c9829683a9e69a05dd1997360b1
KTharushi11 <tharushinimsara702@gmail.com>
2026-03-01 13:53:00 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
-void main(){
-  runApp(DevicePreview(
-      enabled: true,
-      builder: (context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        useInheritedMediaQuery: true,
-        builder: DevicePreview.appBuilder,
-        home: MyApp(),
-      ),
-    ),
-  );
-  
-}
+void main() => runApp(
+  DevicePreview(
+    builder: (context) => MyHomePage(), // Wrap your app
+  ),
+);
 
 
 class MyApp extends StatelessWidget{
